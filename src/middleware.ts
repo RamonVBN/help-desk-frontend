0f631c8e95336fc495eb8f3b3c1ad4416284faf4
RamonVBN <ramon@PC-RAMON.>
2025-09-02 22:21:15 -0300

    deploy: testing middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -31,54 +31,54 @@ export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl
   const token = req.cookies.get("access_token")?.value
 
-  if (pathname === "/") {
-    if (token) {
-      // Usuário autenticado → redireciona para a primeira página "padrão"
-      return NextResponse.redirect(new URL("/calleds", req.url))
-    } else {
-      // Não autenticado → redireciona para login
-      return NextResponse.redirect(new URL(REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE, req.url))
-    }
-  }
+  // if (pathname === "/") {
+  //   if (token) {
+  //     // Usuário autenticado → redireciona para a primeira página "padrão"
+  //     return NextResponse.redirect(new URL("/calleds", req.url))
+  //   } else {
+  //     // Não autenticado → redireciona para login
+  //     return NextResponse.redirect(new URL(REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE, req.url))
+  //   }
+  // }
 
-  // Caso não autenticado e tentando acessar rota privada
-  if (!token && !publicRoutes.includes(req.nextUrl.pathname)) {
-    const redirectUrl = req.nextUrl.clone()
-    redirectUrl.pathname = REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE
-    return NextResponse.redirect(redirectUrl)
-  }
+  // // Caso não autenticado e tentando acessar rota privada
+  // if (!token && !publicRoutes.includes(req.nextUrl.pathname)) {
+  //   const redirectUrl = req.nextUrl.clone()
+  //   redirectUrl.pathname = REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE
+  //   return NextResponse.redirect(redirectUrl)
+  // }
 
-  if (token) {
-    try {
-      const decoded = jwt.decode(token) as { role?: string }
-      const role = decoded?.role
+  // if (token) {
+  //   try {
+  //     const decoded = jwt.decode(token) as { role?: string }
+  //     const role = decoded?.role
 
-      // Bloqueia usuários logados de acessar rotas públicas
-      if (publicRoutes.includes(req.nextUrl.pathname)) {
-        const redirectUrl = req.nextUrl.clone()
-        redirectUrl.pathname = "/"
-        return NextResponse.redirect(redirectUrl)
-      }
+  //     // Bloqueia usuários logados de acessar rotas públicas
+  //     if (publicRoutes.includes(req.nextUrl.pathname)) {
+  //       const redirectUrl = req.nextUrl.clone()
+  //       redirectUrl.pathname = "/"
+  //       return NextResponse.redirect(redirectUrl)
+  //     }
 
-      // Verifica permissões da role
-      if (role && roleRoutes[role]) {
-        const isAllowed = roleRoutes[role].some((pattern) =>
-          pattern.test(req.nextUrl.pathname)
-        )
+  //     // Verifica permissões da role
+  //     if (role && roleRoutes[role]) {
+  //       const isAllowed = roleRoutes[role].some((pattern) =>
+  //         pattern.test(req.nextUrl.pathname)
+  //       )
 
-        if (!isAllowed) {
-          const redirectUrl = req.nextUrl.clone()
-          redirectUrl.pathname = "/calleds"
-          return NextResponse.redirect(redirectUrl)
-        }
-      }
-    } catch (err) {
-      console.error("Erro ao decodificar token:", err)
-      const redirectUrl = req.nextUrl.clone()
-      redirectUrl.pathname = REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE
-      return NextResponse.redirect(redirectUrl)
-    }
-  }
+  //       if (!isAllowed) {
+  //         const redirectUrl = req.nextUrl.clone()
+  //         redirectUrl.pathname = "/calleds"
+  //         return NextResponse.redirect(redirectUrl)
+  //       }
+  //     }
+  //   } catch (err) {
+  //     console.error("Erro ao decodificar token:", err)
+  //     const redirectUrl = req.nextUrl.clone()
+  //     redirectUrl.pathname = REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE
+  //     return NextResponse.redirect(redirectUrl)
+  //   }
+  // }
 
   return NextResponse.next()
 }
