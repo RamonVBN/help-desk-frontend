cea73cac333ff231a72bcdfaac0efe0a6234362c
Ramon Barros <bobsmelhorquemcdonalds@gmail.com>
2026-05-12 21:38:46 -0300

    fix: avatar image loading permission

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3333', // substitua pelo porto do seu servidor
-        pathname: '/**', // permite qualquer caminho
-      },
-    ],
-  },
 };
 
 export default nextConfig;
+
